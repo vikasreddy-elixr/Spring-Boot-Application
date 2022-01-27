0111382bb4877819a1d04dab9071114fab85d658
vikasreddy-elixr <95477225+vikasreddy-elixr@users.noreply.github.com>
2022-01-27 14:34:27 +0530

    Initial Commit

---
+++ springbootapplication/src/test/java/com/elixr/springbootapplication/SpringbootapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elixr.springbootapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
