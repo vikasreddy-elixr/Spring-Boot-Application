d4328855037f20b2abb0b86a7682ab790aa3b5f9
vikasreddy-elixr <vikas.reddy@theelixrlabs.com>
2022-02-04 11:45:38 +0530

    Altered with name of the Exception handling to ExceptionHandler class. removed the unwanted files. added an empty file at end of every file.

--- springbootapplication/src/main/java/com/elixr/springbootapplication/exceptionhandler/ExceptionHandler.java
+++ springbootapplication/src/main/java/com/elixr/springbootapplication/exceptionhandler/ExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.elixr.springbootapplication.exceptionhandler;
 
+
 import com.elixr.springbootapplication.customexceptions.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,45 +8,43 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.NoSuchElementException;
 import static com.elixr.springbootapplication.response.Response.responsesForFalseForCustomException;
 import static com.elixr.springbootapplication.response.Response.responsesForFalseForNoSuchElementException;
 import static com.elixr.springbootapplication.response.Response.responsesForFalseForUnExpectedTypeException;
 
-@RestControllerAdvice
-public class ExceptionHandling {
 
-    @ExceptionHandler(NotFoundException.class)
+
+@RestControllerAdvice
+public class ExceptionHandler {
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(NotFoundException.class)
     public ResponseEntity<?> noSuchDataException() {
         return new ResponseEntity<>(responsesForFalseForCustomException(), HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(NoSuchElementException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<?> noSuchElementException() {
         return new ResponseEntity<>(responsesForFalseForNoSuchElementException(), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> unExpectedTypeException() {
         return new ResponseEntity<>(responsesForFalseForUnExpectedTypeException(), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(BindException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(BindException.class)
     public ResponseEntity<?> bindException() {
         return new ResponseEntity<>(responsesForFalseForUnExpectedTypeException(), HttpStatus.BAD_REQUEST);
     }
-    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ResponseEntity<?> HttpRequestMethodNotSupportedException() {
         return new ResponseEntity<>(responsesForFalseForUnExpectedTypeException(), HttpStatus.BAD_REQUEST);
     }
-    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<?> HttpMessageNotReadableException() {
         return new ResponseEntity<>(responsesForFalseForUnExpectedTypeException(), HttpStatus.BAD_REQUEST);
     }
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<?> IllegalArgumentException() {
-        return new ResponseEntity<>(responsesForFalseForUnExpectedTypeException(), HttpStatus.BAD_REQUEST);
-    }
+
 }
